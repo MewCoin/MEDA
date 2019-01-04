1d9f1c3a43d1ad2d79e998d353191ddd1b03fe20
Syahrul <pauscrypto@gmail.com>
2019-01-04 20:32:29 +0800

    Modified linuxcompile.sh

--- docs/linuxcompile.sh
+++ docs/linuxcompile.sh
@@ -7,7 +7,7 @@ sudo aptitude install -y -o Aptitude::ProblemResolver::SolutionCost='100*cancele
 sudo pip install cmake
 export CC=clang-6.0
 export CXX=clang++-6.0
-git clone -b master --single-branch https://github.com/turtlecoin/turtlecoin
+git clone -b Obsidian-Development --single-branch https://github.com/ObsidianProtocol/obsidian.git
 cd turtlecoin
 mkdir build
 cd build
