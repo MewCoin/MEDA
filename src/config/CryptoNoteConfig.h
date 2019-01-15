19e869a70166e4f4484ad39a37f65532b566dab6
Syahrul <pauscrypto@gmail.com>
2019-01-15 20:12:49 +0800

    Version 4

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -54,7 +54,7 @@ const unsigned EMISSION_SPEED_FACTOR                         = 21;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
 /* Premine amount */
-const uint64_t GENESIS_BLOCK_REWARD                          = UINT64_C(10);
+const uint64_t GENESIS_BLOCK_REWARD                          = UINT64_C(650000000000000);
 
 /* How to generate a premine:
 
@@ -76,7 +76,7 @@ TurtleCoind --print-genesis-tx --genesis-block-reward-address TRTLv2Fyavy8CXG8BP
 * You should see your premine appear in the previously generated wallet.
 
 */
-const char GENESIS_COINBASE_TX_HEX[] = "012801ff00010a02b1253d4a4803d0778adf5e69b256d54af63d687dbfb15c7fe67f41f5aa2dd06f210107f2769ae21a718afec7318d0328470c38dd06520a9aad09cc6132102612a7fb";
+const char GENESIS_COINBASE_TX_HEX[] = "010a01ff000188f3b501029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210142694232c5b04151d9e4c27d31ec7a68ea568b19488cfcb422659a07a0e44dd5";
 static_assert(sizeof(GENESIS_COINBASE_TX_HEX)/sizeof(*GENESIS_COINBASE_TX_HEX) != 1, "GENESIS_COINBASE_TX_HEX must not be empty.");
 
 /* This is the unix timestamp of the first "mined" block (technically block 2, not the genesis block)
