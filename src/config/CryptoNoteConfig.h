c1142b6db0966a6206bbf05e8493497116c99104
Syahrul <pauscrypto@gmail.com>
2019-01-16 15:25:49 +0800

    Emission speed is changed at block 40

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -51,9 +51,9 @@ const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX_V3              = 3;
 const uint64_t LWMA_3_DIFFICULTY_BLOCK_INDEX                 = 1200000;
 
 const unsigned EMISSION_SPEED_FACTOR                          = 9;
-
-const unsigned CHANGE_SPEED_FACTOR                            =2000;
+const unsigned CHANGE_SPEED_FACTOR                            = 30;
 const unsigned EMISSION_SPEED_FACTOR2                         = 21;
+
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
 /* Premine amount */
