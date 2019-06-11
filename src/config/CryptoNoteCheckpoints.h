cf5c28c31b5c7bb44edf37a53a52cbbb05655f70
Syahrul Nizam <muhdsyahrulnizam@Muhds-MacBook-Pro.local>
2019-06-11 09:58:22 +0800

    Added checkpoints for v2.2.0

--- src/config/CryptoNoteCheckpoints.h
+++ src/config/CryptoNoteCheckpoints.h
@@ -26,5 +26,13 @@ struct CheckpointData {
   const char* blockId;
 };
 
-const std::initializer_list<CheckpointData> CHECKPOINTS = {};
+const std::initializer_list<CheckpointData> CHECKPOINTS = {
+  {0,"26cc724fbf60b96b17b74be2a7df726d520a430900f1bc3d516637c07cc46082"},
+  {1000,"c5173b5b02f2dc1a0d2f202a2fd7e47957623b7e0a20acdc672856677b9f0e1e"},
+  {100000,"ea0af04c0084f5a52835f5a783b27252754fa7a02659ef47ea8134f0992f31f7"},
+  {200000,"fe03fa01e00cab9a00c7862d5026a7d4becd8ad6513e8c00c16093a808493a37"},
+  {250000,"f605e98f190363026d0531b8779d20da6469fc3de393f031766f4909026fbd92"},
+  {300000,"35982d60496d285c3d751f874eebdc8b209a3356b9bc73a972e99ece977d028d"},
+  {400000,"3fcb9e9736ce82c280c02c445638124b517d8a8dec2df435e03a79e470a94e89"}
+};
 }
--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -183,6 +183,7 @@ static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 /* Block heights we are going to have hard forks at */
 const uint64_t FORK_HEIGHTS[] =
 {
+    100000,
     2073600,
     4000000,
     6000000  // 0
